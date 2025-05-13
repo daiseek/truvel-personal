58b203ae01641038f8c82fb683d59e7a8e613582
daiseek <dnjsgml0578@gmail.com>
2025-05-13 22:14:47 +0900

    chore: 백엔드 초기 세팅 - docker파일 생성 - spring boot 초기 세팅

---
+++ backend/truvel/src/main/java/alt_t/truvel/TruvelApplication.java
@@ -0,0 +1,13 @@
+package alt_t.truvel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TruvelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TruvelApplication.class, args);
+	}
+
+}
