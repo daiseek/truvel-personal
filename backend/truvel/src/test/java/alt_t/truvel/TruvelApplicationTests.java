aed881bdb0b38da5cefc76b62a2609d412c6da25
daiseek <dnjsgml0578@gmail.com>
2025-05-18 22:08:00 +0900

    chore/#14 : 테스트 db 구축 - 인메모리 방식으로 h2 데이터베이스 설정

--- backend/truvel/src/test/java/alt_t/truvel/TruvelApplicationTests.java
+++ backend/truvel/src/test/java/alt_t/truvel/TruvelApplicationTests.java
@@ -2,8 +2,10 @@ package alt_t.truvel;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TruvelApplicationTests {
 
 	@Test
