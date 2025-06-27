05e4b2f50a6e48ea3317b9e6f3dbacf85e2b6a7b
Jimin15 <sujean07015@gmail.com>
2025-06-27 14:32:26 +0900

    feat: 장소 검색 API 컨트롤러 구현 #24

---
+++ backend/truvel/src/main/java/alt_t/truvel/location/controller/LocationController.java
@@ -0,0 +1,28 @@
+package alt_t.truvel.location.controller;
+
+import alt_t.truvel.location.locationDto.response.LocationResponseDto;
+import alt_t.truvel.location.locationDto.request.LocationSaveRequestDto;
+import alt_t.truvel.location.locationDto.response.GooglePlaceResultDto;
+import alt_t.truvel.location.service.LocationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/locations")
+public class LocationController {
+
+    private final LocationService locationService;
+
+    // 장소 검색
+    @GetMapping("/search")
+    public ResponseEntity<?> searchPlaces(@RequestParam String query) {
+        List<GooglePlaceResultDto> results = locationService.searchPlaces(query);
+
+        return ResponseEntity.ok(results);
+    }
+
+}
