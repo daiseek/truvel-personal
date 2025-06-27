190bba705631f5d737e0a724bb3e0180bcc12319
Jimin15 <sujean07015@gmail.com>
2025-06-27 14:24:34 +0900

    feat: 장소 정보 엔티티 Location 정의 #24

--- backend/truvel/src/main/java/alt_t/truvel/location/domain/entity/Location.java
+++ backend/truvel/src/main/java/alt_t/truvel/location/domain/entity/Location.java
@@ -1,31 +1,32 @@
-package alt_t.truvel.location;
+package alt_t.truvel.location.domain.entity;
 
 import alt_t.truvel.travelPlan.TravelPlan;
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@AllArgsConstructor
 public class Location {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
     private String place;
-
-    @Column
     private Float latitude;
-
-    @Column
     private Float longitude;
+    private String address;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "travel_plan_id")
     private TravelPlan travelPlan;
 
-
     public void setTravelPlan(TravelPlan travelPlan) {
+        this.travelPlan = travelPlan;
     }
+
+
 }
